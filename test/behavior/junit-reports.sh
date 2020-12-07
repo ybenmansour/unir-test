30783d81bfdf4bde36daad430b5ef3a15254fe07
Roberto Santamaria Ayuso <roberto.santamaria@centurylink.com>
2020-12-07 17:13:33 +0100

    aplicar nomenclatura de otros informes junit

--- test/behavior/junit-reports.sh
+++ test/behavior/junit-reports.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for file in $(ls results/*features*.xml);
+for file in calculator calculator-scientific;
 do
-    junit2html $file
+    mv results/TESTS-features.${file}.xml results/behavior_${file}_results.xml
+    junit2html results/behavior_${file}_results.xml results/behavior_${file}_results.html
 done
